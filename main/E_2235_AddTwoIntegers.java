f465b8c0e65594e6fa37f69e5dbd303ed83c96ca
Ernie <yerniyaz.my20@gmail.com>
2024-03-05 18:17:03 +0500

    problem 2235 solved

---
+++ main/E_2235_AddTwoIntegers.java
@@ -0,0 +1,7 @@
+package main;
+
+public class E_2235_AddTwoIntegers {
+    public int sum(int num1, int num2) {
+        return num1 + num2;
+    }
+}
---
+++ test/Test.java
@@ -0,0 +1,11 @@
+package test;
+
+import main.*;
+
+public class Test {
+    public static void main(String[] args) {
+        E_2235_AddTwoIntegers test = new E_2235_AddTwoIntegers();
+        System.out.println(test.sum(3,4));
+    }
+
+}
