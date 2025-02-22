ddd8f29f8d7169a12759222738abf044cccb707b
Ernie <yerniyaz.my20@gmail.com>
2025-02-22 16:48:02 +0500

    Solved E_771_JewelsAndStones

--- test/Test.java
+++ test/Test.java
@@ -4,20 +4,14 @@ import main.*;
 
 public class Test {
     public static void main(String[] args) throws InterruptedException {
-        M_2_AddTwoNumbers test = new M_2_AddTwoNumbers();
 
-        ListNode listNode1 = new ListNode(2);
-        ListNode listNode2 = new ListNode(4);
-        ListNode listNode3 = new ListNode(3);
-        listNode1.next = listNode2;
-        listNode2.next = listNode3;
+        E_771_JewelsAndStones test = new E_771_JewelsAndStones();
 
-        ListNode listNode4 = new ListNode(5);
-        ListNode listNode5 = new ListNode(6);
-        ListNode listNode6 = new ListNode(4);
-        listNode4.next = listNode5;
-        listNode5.next = listNode6;
+        String jewels = "aA";
+        String stones = "aAAbbbb";
 
-        System.out.println(test.addTwoNumbers(listNode1, listNode4).val);
+
+
+        System.out.println(test.numJewelsInStones(jewels, stones));
     }
 }
